84bc2f90bd78c83dde7968a902409fccab6d522d
aseigneurin <alexis.seigneurin@gmail.com>
2010-03-15 16:17:43 +0000

    - integrating patch for issue #38: Add a setting for the refresh interval)

---
+++ HudsonTrayTracker/Entities/GeneralSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hudson.TrayTracker.Entities
+{
+    public class GeneralSettings
+    {
+        public int RefreshTimeInSeconds { get; set; }
+    }
+}
